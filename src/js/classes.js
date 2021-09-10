c6044ab88bd48dfa7e2dcf025db462eb161cc656
VirajS00 <virajshukla00@gmail.com>
2021-09-10 17:59:37 +0530

    initial commit

---
+++ src/js/classes.js
@@ -0,0 +1,10 @@
+class stone {
+	constructor(input) {
+		this.input = input;
+	}
+	show() {
+		console.log(this.input);
+	}
+}
+
+export default stone;
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+import stone from './classes';
+
+const stoneAction = new stone('scissor');
+stoneAction.show();
